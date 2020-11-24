7668cdcd68b9c84acb333ff41d0f5f7634569409
Julek <DoJulka@gmail.com>
2020-11-24 20:06:07 +0100

    Initial commit

---
+++ src/test/java/webapp/webapp/WebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package webapp.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
