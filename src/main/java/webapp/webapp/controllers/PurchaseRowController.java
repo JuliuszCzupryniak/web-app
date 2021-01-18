bccb96e395b5a5754b1d99ac049636020100469b
Julek <DoJulka@gmail.com>
2021-01-18 13:09:38 +0100

    Controllers and HTTPs methods.

--- src/main/java/webapp/webapp/controllers/PurchaseRowController.java
+++ src/main/java/webapp/webapp/controllers/PurchaseRowController.java
@@ -3,12 +3,11 @@ package webapp.webapp.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import webapp.webapp.dtos.PurchaseRequest;
+import webapp.webapp.dtos.PurchaseRowRequest;
 import webapp.webapp.dtos.PurchaseRowResponse;
+import webapp.webapp.entities.PurchaseRow;
 import webapp.webapp.services.PurchaseRowService;
 
 import java.util.List;
@@ -27,5 +26,23 @@ public class PurchaseRowController {
     @GetMapping("/purchase_rows")
     public ResponseEntity<List<PurchaseRowResponse>> getAllPurchaseRows(@RequestBody PurchaseRequest purchaseRequest){
         return new ResponseEntity<>(purchaseRowService.getCustomersPurchaseRows(purchaseRequest), HttpStatus.OK);
+    }
+
+    @PostMapping("/purchase_rows")
+    public ResponseEntity addPurchaseRow(@RequestBody PurchaseRowRequest purchaseRowRequest){
+        purchaseRowService.addPurchaseRow(purchaseRowRequest);
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PatchMapping("/purchase_rows")
+    public ResponseEntity updatePurchaseRow(@RequestBody PurchaseRowRequest purchaseRowRequest){
+        purchaseRowService.updatePurchaseRow(purchaseRowRequest);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/purchase_rows")
+    public ResponseEntity deletePurchaseRow(@RequestBody PurchaseRowRequest purchaseRowRequest){
+        purchaseRowService.deletePurchaseRow(purchaseRowRequest);
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
